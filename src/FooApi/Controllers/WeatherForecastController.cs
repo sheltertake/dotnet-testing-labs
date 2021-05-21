887be403118b3da09968be2686890da100eed12f
Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>
2021-05-21 14:44:58 +0200

    moq

--- src/FooApi/Controllers/WeatherForecastController.cs
+++ src/FooApi/Controllers/WeatherForecastController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FooApi.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace FooApi.Controllers
 {
@@ -10,29 +11,24 @@ namespace FooApi.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
+        private readonly IWeatherService _weatherService;
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+
+            IWeatherService weatherService,
+            ILogger<WeatherForecastController> logger
+            )
         {
+            _weatherService = weatherService;
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get([FromQuery] int num = 5)
+        public async Task<IEnumerable<WeatherForecast>> GetAsync([FromQuery] int num = 5)
         {
-            var rng = new Random();
-            return Enumerable.Range(1, num).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var ret = await _weatherService.GetAsync(num);
+            return ret.ToArray();
         }
     }
 }
