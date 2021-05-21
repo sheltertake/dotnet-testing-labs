22d398861faa54e4c4e0b8a1260ff367eb56e1fc
takeshelter <35832531+sheltertake@users.noreply.github.com>
2021-05-21 14:17:27 +0200

    nunit (#3)
    Co-authored-by: Massimiliano Balestrieri <massimiliano.balestrieri@deltatre.it>

---
+++ tests/FooApiFunctionalTests/Helpers/TestHelper.cs
@@ -0,0 +1,44 @@
+﻿using FooApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace FooApiFunctionalTests.Helpers
+{
+    [ExcludeFromCodeCoverage]
+    public static class TestHelper
+    {
+        public static async Task<IHost> GetTestHostAsync()
+        {
+            // arrange
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+
+
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebHost(webHost =>
+                {
+                    webHost.UseStartup<Startup>();
+                    webHost.UseConfiguration(configuration);
+                    webHost.UseTestServer();
+                    webHost.ConfigureTestServices(services =>
+                    {
+                        services.AddLogging(x => x.AddConsole());
+                        //services.AddAuthentication(FakeJwtBearerDefaults.AuthenticationScheme).AddFakeJwtBearer();
+
+                    });
+                });
+            var host = await hostBuilder.StartAsync();
+            return host;
+        }
+
+    }
+}
--- tests/FooApiUnitTests/Tests/Controller/WeatherForecastControllerTests.cs
+++ tests/FooApiUnitTests/Tests/Controller/WeatherForecastControllerTests.cs
@@ -4,6 +4,7 @@ using FooApi.Controllers;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace FooApiUnitTests.Tests.Controller
@@ -13,20 +14,72 @@ namespace FooApiUnitTests.Tests.Controller
         [SetUp]
         public void Setup()
         {
+            Console.WriteLine(nameof(Setup));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine(nameof(TearDown));
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            Console.WriteLine(nameof(OneTimeTearDown));
+        }
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Console.WriteLine(nameof(OneTimeSetUp));
+        }
+
+        [Category("read")]
+        [TestCase(1)]
+        [TestCase(5)]
+        public void GetTest_ShouldReturnArrayOfWeather(int expected)
+        {
+            var sut = GetTest(expected);
+
+
+            // assert
+            sut.Should().HaveCount(expected);
         }
 
         [Test]
-        public void GetTest_ShouldReturnArrayOfWeather()
+        public void GetTest_ShouldReturnRandom([Random(1, 6, 3)] int expected)
         {
+            var sut = GetTest(expected);
+
+
+            // assert            
+            sut.Should().HaveCountLessOrEqualTo(5);
+        }
+
+        [Test]
+        public void GetTest_ShouldReturnRange([Range(1, 5)] int expected)
+        {
+            var sut = GetTest(expected);
+
+
+            // assert            
+            sut.Should().HaveCountLessOrEqualTo(5);
+        }
+
+
+        private IEnumerable<WeatherForecast> GetTest(int expected)
+        {
+
             // arrange
             var controller = new WeatherForecastController(Mock.Of<ILogger<WeatherForecastController>>());
             // act
-            var sut = controller.Get();
+            var sut = controller.Get(expected);
 
             // assert
             sut.Should().NotBeNull();
             sut.Should().BeOfType<WeatherForecast[]>();
             sut.Should().HaveCountGreaterThan(0);
+            return sut;
         }
     }
 }
